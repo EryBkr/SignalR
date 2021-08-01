93981f354b8d487a7ddfa3e7cadc3ae1bd30f038
Eray Bakır <info@eraybakir.com>
2021-08-01 15:57:30 +0300

    SignalR fundamentals was added

---
+++ SignalR.API/SignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Hubs
+{
+    //Implemente ettik.Metot isimlerimizi magic stringler ile yazmak hataya sebebiyet verebilir
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task SendProduct(Product product)
+        {
+            await Clients.All.ReceiveProduct(product);
+        }
+    }
+}
